ac5840890ef36d7657c807596fb0530589e8582b
Angel <juliojunior11@hotmail.com>
2024-04-26 19:35:25 -0400

    creando archivo para probar volver a un commit anterior

---
+++ client/src/components/ModalC.jsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import CloseIcon from "@mui/icons-material/Close";
+import { Col, Row, Form } from "antd";
+
+export default function ModalC({ open, actBotonClose, titulo, feligres }) {
+  return (
+    <Modal
+      open={open}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      sx={modalstyle}
+    >
+      <Box sx={style}>
+        <ButtonClose>
+          <CloseIcon />
+        </ButtonClose>
+      </Box>
+    </Modal>
+  );
+}
+
+//#region ESTILOS
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "60%",
+  backgroundColor: "#f4f4f4",
+  boxShadow: 2,
+  p: 4,
+  borderRadius: 3,
+};
+
+const modalstyle = {
+  "& .MuiModal-backdrop": {
+    backgroundColor: "#00000033",
+  },
+};
+
+const ButtonClose = styled(IconButton)(() => ({
+  color: "#fff",
+  backgroundColor: "#ff0000",
+  "&:hover": {
+    backgroundColor: "#df5e5e",
+  },
+}));
